2098bdd71903bdbd4e39231dda31ea67432f1df3
dprasanthkumar <110323686+dprasanthkumar@users.noreply.github.com>
2022-09-23 22:37:09 +0530

    loops_concept

---
+++ Geekster/loops/L_b_Loop_desc.java
@@ -0,0 +1,11 @@
+package Geekster;
+
+public class L_b_Loop_desc {
+    public static void main(String[] args)
+    {
+        for(int i=10;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
